165b69ac96698279c90837df2173a956ae938d5b
Vinayak hegde <vhegde@wundermancommerce.com>
2020-06-12 17:32:25 +0100

    initial set up

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './public/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  mode: 'development',
+  entry: ['./src/index.tsx'],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s?css$/i,
+        use: ['style-loader', 'css-loader?sourceMap=true', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+};
